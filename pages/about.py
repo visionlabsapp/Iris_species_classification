6a750edd44c6ca997271a508a2bf9ed15f3d989b
visionlabsapp <107917467+visionlabsapp@users.noreply.github.com>
2022-07-18 14:52:08 +0800

    Add files via upload

--- pages/about.py
+++ pages/about.py
@@ -17,17 +17,15 @@ def app():
 
     with img_col:
         # Add your photo here
-        st.image("./images/my_photo.png")
+        st.image("./images/eddiechow.png")
         pass
 
     with content_col:
         # cust
         st.markdown("""<br><br><br>""", unsafe_allow_html=True)
         # Add your name 
-        st.markdown('''### Name: [Shishir Shekhar]()''')
-        # Add your email
-        st.markdown('''### Email: [ShishirShekhar](mailto:sspdav02@gmail.com?subject=Iris%20Species%20Classifier&body=Hi%2C%20I%20saw%20your%20Iris%20Species%20Classifier.%0AI%20wanted%20to%20share%20my%20feedback%20with%20you.%0A%0AWrite%20your%20feedback%20here....)''')
+        st.markdown('''### Name: [Eddie Chow()''')
         # Add your github
-        st.markdown('''### GitHub: [ShishirShekhar](https://github.com/ShishirShekhar/)''')
+        st.markdown('''### GitHub: [Eddie Chow](https://github.com/eddiecityu)''')
         # Add your linkedin
-        st.markdown('''### Linkedin: [Shishir-Shekhar](https://www.linkedin.com/in/shishir-shekhar/)''')
+        st.markdown('''### Linkedin: [Eddie Chow](https://www.linkedin.com/in/eddie-chow-a3860464/)''')
